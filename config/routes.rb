fd3e4e56cc0a94606a9ef02e6017ba745a5190d9
pluto168 <86468019+pluto168@users.noreply.github.com>
2023-02-15 00:21:49 +0800

    2.8 - 首頁 Layout (電子報訂閱、商品列表、商品內頁)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'products#index'  #會有products的controller,裏面會有index的action,同時會有index的view
+
+  resources :products, only: [:index, :show] #products在前端不需要全部路徑
 end
