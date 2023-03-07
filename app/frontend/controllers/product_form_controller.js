49cacb191b4b957bc20ec9ff57c8bd9ef38ff0b3
pluto168 <86468019+pluto168@users.noreply.github.com>
2023-03-07 20:10:13 +0800

    2.27 - 商品庫存 part 4

--- app/frontend/controllers/product_form_controller.js
+++ app/frontend/controllers/product_form_controller.js
@@ -10,7 +10,16 @@ export default class extends Controller {
                   this.linkTarget.insertAdjacentHTML('beforebegin', content);
 
   }
-  connect() {
-  }
 
+  remove_sku(event){
+    event.preventDefault();
+
+    let wrapper = event.target.closest('.nested-fields');
+    if(wrapper.dataset.newRecord == 'true'){
+      wrapper.remove();
+    }else{
+      wrapper.querySelector("input[name*='_destroy']").value = 1;
+      wrapper.style.display = 'none';
+    }
+  }
 }
