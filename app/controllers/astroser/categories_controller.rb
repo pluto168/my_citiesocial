f5912b56e1a839e51694e2bab2dc385f5aa3ddea
pluto168 <86468019+pluto168@users.noreply.github.com>
2023-03-13 13:41:00 +0800

    2.32 - 商品分類 part 3

--- app/controllers/astroser/categories_controller.rb
+++ app/controllers/astroser/categories_controller.rb
@@ -1,6 +1,8 @@
 class Astroser::CategoriesController < Astroser::BaseController
+  before_action :find_category, only: [:edit, :update, :destroy]
   
   def index
+    @category = Category.all.order(position: :asc)       #all可以省略
   end
 
   def new
@@ -10,14 +12,36 @@ class Astroser::CategoriesController < Astroser::BaseController
   def create
     @category = Category.new(category_params)
     if @category.save
-      redirect_to astroser_categories_path, notice: '新增分類成功'
+      redirect_to astroser_categories_path, notice: '分類新增成功'
     else
       render :new
     end
   end
 
+  def edit
+    #@category=Product.find(params[:id])
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to astroser_categories_path, notice: '分類已更新'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    #@category=Product.find(params[:id])
+    @category.destroy
+    redirect_to astroser_categories_path, notice: '分類已刪除'
+  end
+
   private
   def category_params
     params.require(:category).permit(:name)
+  end
+
+  def find_category
+    @category=Category.find(params[:id])
   end
 end
