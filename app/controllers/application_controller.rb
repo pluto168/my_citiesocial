f1bb222052d6079f41e4e2a2a95b26ce25ac6d2c
pluto168 <86468019+pluto168@users.noreply.github.com>
2023-03-13 22:33:17 +0800

    2.35 - 商品分類 part 6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,20 @@ class ApplicationController < ActionController::Base
 
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
+  before_action :find_categories, unless: :backend?  #如果再後台就不撈
+
   private
   def record_not_found
     render  file: "#{Rails.root}/public/404.html",              #{Rails.root}是根目錄
             layout: false,    #不需要layout,例footer或頂端首頁...
             status: 404
+  end
+
+  def backend?
+    controller_path.split('/').first == 'Astroser'
+  end
+
+  def find_categories
+    @categories = Category.order(position: :asc)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'products#index'  #會有products的controller,裏面會有index的action,同時會有index的view
 
+  #前台
   resources :products, only: [:index, :show] #products在前端不需要全部路徑
+  resources :categories, only: [:show]       #/category/2
 
   #後台
   namespace :astroser do
-    root 'products#index'    #/astroser
+    root 'products#index'    # /astroser
     resources :products, except: [:show]
     resources :vendors, except: [:show]   #後台不需show,有新增,修改,刪除,列表
     resources :categories, except: [:show] do
