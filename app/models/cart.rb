36ebc7f5377219b6edf9c3869c57e1215cdb3e01
pluto168 <86468019+pluto168@users.noreply.github.com>
2023-03-17 12:48:48 +0800

    2.41 購物車 part 2

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,10 +1,21 @@
 #PORO = Plain Old Ruby Object
 
 class Cart
+    attr_reader :items
+
+    def initialize
+        @items = []
+    end
+
     def add_item(product_id)
+        @items << product_id    # <<是把product_id丟到陣列@items
     end
 
     def empty?
-        false
+        @items.empty?        #陣列@items是否為空?
     end
+
+    # def items               #attr_reader :items就可以寫出這3行   
+    #     return @items 
+    # end
 end
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -4,16 +4,24 @@ RSpec.describe Cart, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
 
   #describe也可以換成context
-  describe "基本功能" do        
+  context "基本功能" do        
     it "可以把商品丟到到購物車裡，然後購物車裡就有東西了" do
       # AAA三A原則 = Arrange安排,Act操作使用,Assert評估
       cart = Cart.new
       cart.add_item(2)   #2代表商品編號
       expect(cart.empty?).to be false  #expect預期
     end
+
+    it "如果加了相同種類的商品到購物車裡，購買項目（CartItem）並不會增加，但商品的數量會改變。" do
+      cart = Cart.new
+      3.times {cart.add_item(1)}   #加3次購物車
+      2.times {cart.add_item(2)}
+      expect(cart.items.count).to be 2    #測試車子的數量是不是對的
+      expect(cart.items.first.quantity).to be 3     #測試數量是不是3
+      # expect(cart.items.last.quantity).to be 2
+    end
   end 
 
-  describe "進階功能" do
-  end 
-
+  context "進階功能" do
+  end
 end
